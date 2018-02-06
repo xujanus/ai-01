3f960324eab764a6b0ad9dece5fc2085e18fba30
xujun <xujun@zjrealtech.com>
2018-02-06 18:38:18 +0800

    项目初始化，增加了main方法和它的单元测试

---
+++ src/main/java/com/xujun365/Main.java
@@ -0,0 +1,18 @@
+package com.xujun365;
+
+/**
+ * <p>ClassName:     Main
+ * <p>Description:   程序主方法
+ * <p>Author         xujun
+ * <p>Version        V1.0
+ * <p>Date           2018/2/6
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public void test(String args){
+        System.out.println(args);
+    }
+}
---
+++ src/test/java/com/xujun365/MainTest.java
@@ -0,0 +1,21 @@
+package com.xujun365;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * <p>ClassName:     MainTest
+ * <p>Description:   Main方法的测试用例
+ * <p>Author         xujun
+ * <p>Version        V1.0
+ * <p>Date           2018/2/6
+ */
+public class MainTest {
+    @Test
+    public void test() throws Exception {
+        Main main = new Main();
+        main.test("unit test");
+    }
+
+}
